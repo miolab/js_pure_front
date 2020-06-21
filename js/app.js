c6279d712d19f4a57c25b58892a62f6a82e12922
im <poson_hemp@yahoo.co.jp>
2020-06-21 18:49:50 +0900

    style change JS triger (transform to circle)

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 
 const box = document.getElementById("box")
 
-box.addEventListener('click', () =>{
+box.addEventListener('click', () => {
   box.style.background = 'yellow';
+  box.style.borderRadius = '50%';
 });
