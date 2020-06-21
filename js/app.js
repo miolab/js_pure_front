aefc409c294b55e83e1e47f33be1283a96a980c8
im <poson_hemp@yahoo.co.jp>
2020-06-21 19:37:23 +0900

    replicate boxes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 'use strict';
 
-const box = document.getElementById("box")
+for (let i = 0; i < 3; i++) {
+  const divBox = document.createElement('div');
 
-box.addEventListener('click', () => {
-  box.classList.add('yellow-circle')
-});
+  document.body.appendChild(divBox);
+
+  divBox.classList.add('box');
+  divBox.innerHTML = "im " + String(i);
+
+  divBox.addEventListener('click', () => {
+    divBox.classList.toggle('yellow-circle');
+  })
+}
