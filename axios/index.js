e736fddf2aeb6c73113034828670f93dc00badf6
im <poson_hemp@yahoo.co.jp>
2020-06-30 09:41:50 +0900

    init

---
+++ axios/index.js
@@ -0,0 +1,11 @@
+// import {
+//   appAxios
+// } from 'src/app';
+
+// appAxios();
+
+const divElm = document.createElement('div');
+
+document.body.appendChild(divElm);
+
+divElm.innerHTML = "test by im";
